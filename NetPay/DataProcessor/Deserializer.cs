e75f18f7d8f01ad65b27cb0a50be926783d01e74
Viks Georgiev <viks47@gmail.com>
2025-03-22 15:13:18 +0200

    Add project files.

---
+++ NetPay/DataProcessor/Deserializer.cs
@@ -0,0 +1,38 @@
+﻿using NetPay.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetPay.DataProcessor
+{
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data format!";
+        private const string DuplicationDataMessage = "Error! Data duplicated.";
+        private const string SuccessfullyImportedHousehold = "Successfully imported household. Contact person: {0}";
+        private const string SuccessfullyImportedExpense = "Successfully imported expense. {0}, Amount: {1}";
+
+        public static string ImportHouseholds(NetPayContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportExpenses(NetPayContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResults = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(dto, validationContext, validationResults, true);
+
+            foreach(var result in validationResults)
+            {
+                string currvValidationMessage = result.ErrorMessage;
+            }
+
+            return isValid;
+        }
+    }
+}
---
+++ NetPay/DataProcessor/Serializer.cs
@@ -0,0 +1,17 @@
+﻿using NetPay.Data;
+
+namespace NetPay.DataProcessor
+{
+    public class Serializer
+    {
+        public static string ExportHouseholdsWhichHaveExpensesToPay(NetPayContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportAllServicesWithSuppliers(NetPayContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
